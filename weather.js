02f6cc3192d7962f932d278f791a3437165634ea
Legion_Apps <59895080+Moabifokotsane@users.noreply.github.com>
2023-04-13 23:57:18 +0200

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,22 +1,31 @@
 function getWeather() {
-  const apiKey = 'Y6ew5Z5bbig9Abk0WikGjPbrtLJFckcW';
-  const locationKey = '123950';
-  const url = `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`;
+  // Replace YOUR_API_KEY_HERE with your OpenWeatherMap API key
+  let apiKey = "156378f4f424064b01eb3fc95bd05da0";
+  
+  // Replace CITY_NAME_HERE with the name of the city you want weather information for
+  let city = "Gaborone";
+  
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      const temperature = data[0].Temperature.Metric.Value;
-      const description = data[0].WeatherText;
-      const windSpeed = data[0].Wind.Speed.Metric.Value;
-      const humidity = data[0].RelativeHumidity;
-
-      document.getElementById('temperature').textContent = temperature;
-      document.getElementById('description').textContent = description;
-      document.getElementById('wind-speed').textContent = windSpeed;
-      document.getElementById('humidity').textContent = humidity;
+      let weather = {};
+      weather.temperature = data.main.temp;
+      weather.description = data.weather[0].description;
+      weather.icon = data.weather[0].icon;
+      displayWeather(weather);
     })
     .catch(error => {
-      console.error('Error:', error);
+      console.log("Error fetching weather data: ", error);
     });
+}
+
+function displayWeather(weather) {
+  // Replace temperature, description, and icon with the corresponding HTML element IDs in your web page
+  document.getElementById("temperature").textContent = weather.temperature;
+  document.getElementById("description").textContent = weather.description;
+  // The weather icon is returned as a code (e.g. "01d" for clear sky day). You can use an icon font or an image file to display the weather icon.
+  // Here's an example of how to display the icon using the OpenWeatherMap icon font:
+  document.getElementById("icon").className = `owf owf-${weather.icon}`;
 }

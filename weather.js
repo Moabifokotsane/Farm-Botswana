f6dc00892e347685633d13a5b07253ff81e4866e
Legion_Apps <59895080+Moabifokotsane@users.noreply.github.com>
2023-04-13 23:40:28 +0200

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,19 +1,20 @@
 function getWeather() {
-  fetch('http://www.weather.gov.bw/')
-    .then(response => response.text())
+  const apiKey = 'Y6ew5Z5bbig9Abk0WikGjPbrtLJFckcW';
+  const locationKey = '123950';
+  const url = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`;
+
+  fetch(url)
+    .then(response => response.json())
     .then(data => {
-      const parser = new DOMParser();
-      const htmlDocument = parser.parseFromString(data, 'text/html');
+      const temperature = data[0].Temperature.Metric.Value;
+      const description = data[0].WeatherText;
+      const windSpeed = data[0].Wind.Speed.Metric.Value;
+      const humidity = data[0].RelativeHumidity;
 
-      const temperature = htmlDocument.querySelector('.temperature-value').textContent;
-      const description = htmlDocument.querySelector('.weather-description').textContent;
-      const windSpeed = htmlDocument.querySelector('.wind-speed').textContent;
-      const humidity = htmlDocument.querySelector('.humidity').textContent;
-
-      console.log('Temperature:', temperature);
-      console.log('Description:', description);
-      console.log('Wind Speed:', windSpeed);
-      console.log('Humidity:', humidity);
+      document.getElementById('temperature').textContent = temperature;
+      document.getElementById('description').textContent = description;
+      document.getElementById('wind-speed').textContent = windSpeed;
+      document.getElementById('humidity').textContent = humidity;
     })
     .catch(error => {
       console.error('Error:', error);
